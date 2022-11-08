67390c2552062caee119b8d0f062b58a399fd2b4
trevisliu@gmail.com <>
2022-11-09 00:05:52 +0530

    Camera now supports panning with the mouse.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,8 +6,10 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject follow;
     public Vector3 offset = new Vector3(0f, 1f, -5f);
+    public Vector2 maxRotation = new Vector2(5f, 5f);
 
     public float smoothTime = .3f;
+    public bool snap = false;
     public bool trackXAxis = true;
     public bool trackYAxis = true;
     public bool trackZAxis = true;
@@ -23,8 +25,14 @@ public class CameraMovement : MonoBehaviour
             (trackZAxis) ? followVec.z : this.transform.position.z
         );
 
-        this.transform.position = Vector3.SmoothDamp(
+        this.transform.position = (this.snap) ? destination : Vector3.SmoothDamp(
             this.transform.position, destination, ref velocity, smoothTime
+        );
+
+        this.transform.rotation = Quaternion.Euler(
+            maxRotation.x * (Mathf.Min(1f, Mathf.Max(0f, Input.mousePosition.y / Screen.height)) * 2 - 1) * -1,
+            maxRotation.y * (Mathf.Min(1f, Mathf.Max(0f, Input.mousePosition.x / Screen.width)) * 2 - 1),
+            0f
         );
     }
 }
